143be1f0af363a9135313f1c1c43f4457e12802a
zaramid <piotr@zawadzki.net>
2017-03-12 18:00:17 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function rysujChoinke(wys) {
+	for (var n = 1; n <=wys; n++) {
+		star = "";
+		for (var i = wys; i > n; i--) {
+			star += " ";
+		}
+		for (var i = 1; i <= n*2-1; i++) {
+			star += "*";
+		}
+		console.log(star);
+	}
+}
+
+rysujChoinke(10);
